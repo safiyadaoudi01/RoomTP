095c90c22f38eb01b09d1ae14122c4fed7593a3a
safiyadaoudi01 <safidaou33@gmail.com>
2023-12-14 21:30:52 +0000

    summary
    12/14/2023

---
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.roomwordsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.roomwordsample.adapter.WordListAdapter;
+import com.example.roomwordsample.databinding.ActivityMainBinding;
+import com.example.roomwordsample.model.Word;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+    private ActivityMainBinding binding;
+    private WordViewModel mWordViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        WordListAdapter mAdapter = new WordListAdapter();
+        binding.contentMain.recyclerview.setAdapter(mAdapter);
+        binding.contentMain.recyclerview.setHasFixedSize(true);
+
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+        mWordViewModel.getAllWords().observe(this, words -> {
+            // Mettre à jour la copie en cache des mots dans l'adaptateur.
+            mAdapter.setWords(words);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}
